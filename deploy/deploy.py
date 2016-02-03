00f992bac6293b00e63f75eb3f81a2a2d53a8052
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2016-02-03 02:02:34 +0000

    Updated hirs_tpw_orbital/deploy/deploy.py to v20160114

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -8,7 +8,7 @@ LOG = logging.getLogger(__file__)
 class HIRS_TPW_Orbital_Package(Package):
 
     def deploy_package(self):
-        for version in ['v20150915']:
+        for version in ['v20160114']:
             self.merge(Extracted('HIRS_TPW_Orbital_{}.tar.gz'.format(version)).path(), version)
             self.merge(NetcdfFortran().path(), version)
             self.merge(Netcdf().path(), version)
