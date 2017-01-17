1b7974db6240046041bfdb291efec43e4bdf890c
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2017-01-17 06:09:18 +0000

    Updated cluster submission scripts for HIRS processing, mainly to take advantage of the new safe submission routine, which transparently manages job submission so that things don't get bogged down.

--- submit_hirs_tpw_orbital.py
+++ submit_hirs_tpw_orbital.py
@@ -1,69 +1,100 @@
-from datetime import datetime
-from flo.time import TimeInterval
-from flo.sw.hirs_ctp_orbital import HIRS_CTP_ORBITAL
-from flo.sw.hirs_tpw_orbital import HIRS_TPW_ORBITAL
-from flo.ui import submit_order
-from flo.ui import local_prepare, local_execute
-import logging
+import os
 import sys
 import time
+import re
+import string
+from datetime import datetime, timedelta
+from calendar import monthrange
+import logging
+import traceback
+import getpass
+
+from subprocess import CalledProcessError, call
+from subprocess import Popen, STDOUT, PIPE
+
+from flo.time import TimeInterval
+from flo.ui import safe_submit_order
+from flo.product import StoredProductCatalog
+
+from flo.sw.hirs_ctp_orbital import HIRS_CTP_ORBITAL
+from flo.sw.hirs_tpw_orbital import HIRS_TPW_ORBITAL
 
 # every module should have a LOG object
-import logging, traceback
 LOG = logging.getLogger(__file__)
 
-def _local_execute_example(platform,interval):
-    '''
-    Run this computation locally
-    '''
+# Set up the logging
+console_logFormat = '%(asctime)s : (%(levelname)s):%(filename)s:%(funcName)s:%(lineno)d:  %(message)s'
+dateFormat = '%Y-%m-%d %H:%M:%S'
+levels = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
+logging.basicConfig(stream=sys.stdout, level=levels[0],
+                    format=console_logFormat,
+                    datefmt=dateFormat)
 
-    hirs_version = 'v20151014'
-    collo_version = 'v20140204'
-    csrb_version = 'v20150915'
-    ctp_version = 'v20150915'
-    tpw_version = 'v20150915'
+# General information
+comp = HIRS_TPW_ORBITAL()
+SPC = StoredProductCatalog()
 
-    c = HIRS_TPW_ORBITAL()
-    contexts = c.find_contexts(platform, hirs_version, collo_version, csrb_version, ctp_version,
+# Latest Computation versions.
+hirs_version  = 'v20151014'
+collo_version = 'v20151014'
+csrb_version  = 'v20150915'
+ctp_version   = 'v20150915'
+tpw_version   = 'v20160222'
+
+platform_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
+                    'noaa-12', 'noaa-14', 'noaa-15', 'noaa-16', 'noaa-17', 'noaa-18',
+                    'noaa-19', 'metop-a', 'metop-b']
+
+platform = 'metop-b'
+
+# Specify the intervals
+wedge = timedelta(seconds=1.)
+#intervals = [
+    #TimeInterval(datetime(2016, 1, 1), datetime(2016, 2, 1) - wedge),
+    #TimeInterval(datetime(2016, 2, 1), datetime(2016, 3, 1) - wedge),
+    #TimeInterval(datetime(2016, 3, 1), datetime(2016, 4, 1) - wedge),
+    #TimeInterval(datetime(2016, 4, 1), datetime(2016, 5, 1) - wedge),
+    #TimeInterval(datetime(2016, 5, 1), datetime(2016, 6, 1) - wedge),
+    #TimeInterval(datetime(2016, 6, 1), datetime(2016, 7, 1) - wedge),
+    #TimeInterval(datetime(2016, 7, 1), datetime(2016, 8, 1) - wedge),
+    #TimeInterval(datetime(2016, 8, 1), datetime(2016, 9, 1) - wedge),
+    #TimeInterval(datetime(2016, 9, 1), datetime(2016, 10, 1) - wedge),
+    #TimeInterval(datetime(2016, 10, 1),datetime(2016, 11, 1) - wedge),
+    #TimeInterval(datetime(2016, 11, 1),datetime(2016, 12, 1) - wedge),
+    #TimeInterval(datetime(2016, 12, 1),datetime(2017, 1, 1) - wedge),
+#]
+
+# Examine how many of the defined contexts are populated
+intervals = []
+year,month = 2016,4
+days = range(1,monthrange(year, month)[1]+1)
+for day in days:
+    dt_start = datetime(year, month, day)
+    dt_end = datetime(year, month, day) + timedelta(days = 1)
+    interval = TimeInterval(dt_start, dt_end - wedge)
+    contexts = comp.find_contexts(platform, hirs_version, collo_version, csrb_version, ctp_version,
                                tpw_version, interval)
-    local_prepare(comp, contexts[0])
-    local_execute(comp, contexts[0])
+    num_contexts_exist = 0
+    for context in contexts:
+        num_contexts_exist += SPC.exists(comp.dataset('out').product(context))
+    LOG.info("Interval {} has {}/{} contexts existing".format(interval, num_contexts_exist, len(contexts)))
+    missing_contexts = len(contexts) - num_contexts_exist
+    if missing_contexts > 3:
+        intervals.append(interval)
 
 
-def submit(logger, interval, platform):
-    '''
-    Submit this computation to the cluster.
-    '''
-
-    hirs_version = 'v20151014'
-    collo_version = 'v20140204'
-    csrb_version = 'v20150915'
-    ctp_version = 'v20150915'
-    tpw_version = 'v20150915'
-
-    c = HIRS_TPW_ORBITAL()
-    contexts = c.find_contexts(platform, hirs_version, collo_version, csrb_version, ctp_version,
+LOG.info("Submitting intervals...")
+for interval in intervals:
+    LOG.info("Submitting interval {} -> {}".format(interval.left, interval.right))
+    contexts = comp.find_contexts(platform, hirs_version, collo_version, csrb_version, ctp_version,
                                tpw_version, interval)
-
-    while 1:
-        try:
-            return submit_order(c, [c.dataset('out')], contexts, (HIRS_CTP_ORBITAL(),))
-        except:
-            logger.info('Failed submiting jobs.  Sleeping for 5 minutes and submitting again')
-            time.sleep(5*60)
-
-# Setup Logging
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='[%(asctime)s] %(message)s')
-logger = logging.getLogger(__name__)
-
-# Submitting Jobs
-for platform in ['metop-a']:
-    for interval in [TimeInterval(datetime(2009, 1, 1), datetime(2009, 2, 1))]:
-        jobIDRange = submit(logger, interval, platform)
-
-        if len(jobIDRange) > 0:
-            logger.info('Submitting hirs_tpw_orbital jobs for {} '.format(platform) +
-                        'from {} to {}'.format(interval.left, interval.right))
-        else:
-            logger.info('No hirs_tpw_orbital jobs for {} '.format(platform) +
-                        'from {} to {}'.format(interval.left, interval.right))
+    LOG.info("\tThere are {} contexts in this interval".format(len(contexts)))
+    contexts.sort()
+    #for context in contexts:
+        #LOG.debug(context)
+    LOG.info("\tFirst context: {}".format(contexts[0]))
+    LOG.info("\tLast context:  {}".format(contexts[-1]))
+    LOG.info("\t{}".format(safe_submit_order(comp,
+                                             [comp.dataset('out')],
+                                             contexts,
+                                             download_onlies=[HIRS_CTP_ORBITAL()])))
