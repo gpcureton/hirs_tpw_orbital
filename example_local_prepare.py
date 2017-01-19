4982ed14f2d0650384203860f5ce623c08f5c782
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2017-01-19 22:10:04 +0000

    Rationalised the various HIRS local submission scripts.

--- example_local_prepare.py
+++ example_local_prepare.py
@@ -1,6 +1,8 @@
-import os,sys
-import time
-from datetime import datetime
+import os
+from datetime import datetime, timedelta
+import logging
+import traceback
+
 from flo.time import TimeInterval
 from flo.ui import local_prepare, local_execute
 
@@ -14,51 +16,70 @@ LOG = logging.getLogger(__file__)
 
 
 # Set up the logging
-console_logFormat = '%(asctime)s : (%(levelname)s):%(filename)s:%(funcName)s:%(lineno)d:  %(message)s'
-dateFormat = '%Y-%m-%d %H:%M:%S'
 levels = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
-logging.basicConfig(level = levels[3], 
-        format = console_logFormat, 
-        datefmt = dateFormat)
+level = levels[3]
+if level == logging.DEBUG:
+    console_logFormat = '%(asctime)s.%(msecs)03d (%(levelname)s) : %(filename)s : %(funcName)s : %(lineno)d:%(message)s'
+    dateFormat = '%Y-%m-%d %H:%M:%S'
+else:
+    console_logFormat = '%(asctime)s.%(msecs)03d (%(levelname)s) : %(message)s'
+    dateFormat = '%Y-%m-%d %H:%M:%S'
 
+logging.basicConfig(level=level,
+                    format=console_logFormat,
+                    datefmt=dateFormat)
 
-def local_execute_example(sat, hirs_version, collo_version, csrb_version, ctp_version, tpw_version, granule):
+# General information
+comp = HIRS_TPW_ORBITAL()
+
+#
+# Local execution
+#
+
+def local_execute_example(granule, platform, hirs_version, collo_version, csrb_version, ctp_version,
+                          tpw_version, skip_prepare=False, skip_execute=False):
+    comp_dict = { 
+        'granule': granule
+        'sat': platform, 
+        'hirs_version': hirs_version, 
+        'collo_version': collo_version, 
+        'csrb_version': csrb_version,
+        'ctp_version': ctp_version,
+        'tpw_version': tpw_version}
+
     try:
-        LOG.info("Running local_prepare(HIRS_TPW_ORBITAL(),...)") # GPC
-        local_prepare(HIRS_TPW_ORBITAL(), { 
-                                      'sat': sat, 
-                                      'hirs_version': hirs_version, 
-                                      'collo_version': collo_version, 
-                                      'csrb_version': csrb_version,
-                                      'ctp_version': ctp_version,
-                                      'tpw_version': tpw_version,
-                                      'granule': granule
-                                      },
-                      download_only=[HIRS(), HIRS_CTP_ORBITAL()]
-                     )
-        LOG.info("Running local_execute(HIRS_TPW_ORBITAL(),...)") # GPC
-        local_execute(HIRS_TPW_ORBITAL(), {
-                                      'sat': sat, 
-                                      'hirs_version': hirs_version, 
-                                      'collo_version': collo_version, 
-                                      'csrb_version': csrb_version,
-                                      'ctp_version': ctp_version,
-                                      'tpw_version': tpw_version,
-                                      'granule': granule
-                                      }
-                                      )
-    except Exception, err :
-        LOG.error("{}.".format(err))
-        LOG.debug(traceback.format_exc())
+        if not skip_prepare:
+            LOG.info("Running local prepare...")
+            local_prepare(comp, comp_dict, download_only=[HIRS(), HIRS_CTP_ORBITAL()])
+        if not skip_execute:
+            LOG.info("Running local execute...")
+            local_execute(comp, comp_dict)
+    except Exception, err:
+        LOG.error("{}".format(err))
+        LOG.info(traceback.format_exc())
 
+def print_contexts(platform, dt_left, dt_right, granule_length):
+    interval = TimeInterval(dt_left, dt_right)
+    contexts = comp.find_contexts(platform, hirs_version, collo_version, csrb_version, ctp_version,
+                                  tpw_version, interval)
+    contexts.sort()
+    for context in contexts:
+        print context
 
+    return contexts
 
-sat = 'metop-b'
-hirs_version  = 'v20151014'
+platform_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
+                    'noaa-12', 'noaa-14', 'noaa-15', 'noaa-16', 'noaa-17', 'noaa-18',
+                    'noaa-19', 'metop-a', 'metop-b']
+
+platform = 'metop-b'
+hirs_version = 'v20151014'
 collo_version = 'v20151014'
-csrb_version  = 'v20150915'
+csrb_version = 'v20150915'
 ctp_version = 'v20150915'
 tpw_version = 'v20160222'
-granule = datetime(2014, 1, 15, 0, 0)
+
+granule = datetime(2016, 6, 3, 21, 17)
+granule = datetime(2016, 6, 3, 20, 32)
 
 #local_execute_example(sat, hirs_version, collo_version, csrb_version, ctp_version, tpw_version, granule)
